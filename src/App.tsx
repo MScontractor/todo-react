0219d50d65b4ce33b110ef9a37241d9db9c77f85
Manuel S <manuels@MBP-de-Manuel.fibertel.com.ar>
2022-12-12 22:38:47 -0300

    remove header description

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,6 @@ const App: React.FC = () => {
       <div className="App">
         <header className="App-header">
           <h2>Todo Page</h2>
-          <h5>
-            This is a simple App to show how to provide different state managers and the differences between them.
-            I know that the implementation of some of them could be some overkill but this is  just an example.
-          </h5>
         </header>
         <TodoPage />
       </div>
